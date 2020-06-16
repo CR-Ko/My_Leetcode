006425430bfd292df5412fd66c3608c67405c0cf
cr.ke <crke@digwise-tech.com>
2020-06-16 02:45:08 -0700

    Sliding Window 5 is ok.

--- Sliding_Window/5_Longest_substring_wo_repeating_chars/main.cpp
+++ Sliding_Window/5_Longest_substring_wo_repeating_chars/main.cpp
@@ -3,18 +3,17 @@
 #include <string>
 using namespace std;
 
-
+// This function returns True whem all characters in str[i...j]
+// are distinct, otherwise returns False
 class AreDistinct{
 	public:
 		static bool TwoCharAreDistinct(const string &str, int i, int j){
-			vector<bool>visited(26);
+			vector<bool> visited(26);
 			for(int k=i;k<=j;k++){
-				//if(visited[str[k]-'a'])
-				{
-					cout<<  "str[k]: " <<str[k]<<endl;
-					cout << "visited[str[k]-'a']: "<<visited[str[k]-'a']<<endl;
-
+				if(visited[str[k]-'a']==true){
+					return false;
 				}
+				visited[str[k]-'a']=true;
 			}
 			return true;
 		}
@@ -23,23 +22,29 @@ class AreDistinct{
 
 class LongestLength{
 	public:
-		static int findLength(const string &str){
+		static int findLengthOfLongestUniqueSubstr(const string &str){
 			int n = str.size();
 			int res = 0;
-			int cur = 0;
-			for(int i =0;i<n;i++){
+			cout<<"n: "<<n<<endl;
+			for(int i=0;i<n;i++){
 				for(int j=i;j<n;j++){
 					if(AreDistinct::TwoCharAreDistinct(str, i, j))
 					{
-						res = max(cur, j-i+1);//here
-					}	
+						cout<<res<<endl;
+						res = max(res, j-i+1);//here
+						
+					}
 				}
+			
 			}
-		}
+			return res;
+	}
 };
 
 
 int main(int argc, char* argv[]){
-	bool ans = AreDistinct::TwoCharAreDistinct("abcde", 0, 3);
-	cout<<ans<<endl;
+	string str = "geeksforgeeks";
+	cout<< "The input string is: "<< str<<endl;
+	int len = LongestLength::findLengthOfLongestUniqueSubstr(str);
+	cout<<"Len: "<<len<<endl;
 }
