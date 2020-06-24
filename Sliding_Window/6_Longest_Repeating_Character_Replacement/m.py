cccf2acc2efaebad0650005961e0f7711ad2e84b
cr.ke <crke@digwise-tech.com>
2020-06-23 19:15:27 -0700

    Update: Sliding Window: 6

---
+++ Sliding_Window/6_Longest_Repeating_Character_Replacement/m.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findLongestRepeatingSubstring(self,k,s:
+            ans = 0
+            new = []
+            r = 0
+            l = 0
+            while r < len(s) and l < len(s):
+                 
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    k = 2
+    s = 'ABAB'
+    print(sol.findLongestRepeatingSubstring(k,s))
