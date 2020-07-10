f408d4250fdb050b7aa58ac7e3e1fb29c803a2aa
cr.ke <crke@digwise-tech.com>
2020-07-10 06:10:04 -0700

    Update

--- Sliding_Window/7_Longest_Substring_with_At_Most_Two_Distinct_Characters/m.py
+++ Sliding_Window/7_Longest_Substring_with_At_Most_Two_Distinct_Characters/m.py
@@ -1,15 +1,33 @@
 class Solution(object):
-    def findLength(arr):
+    def findLength(self, arr):
         """
-        type:
-        rtype:
+        type:  str
+        rtype: int
         """
+        # Caterpillar
+        m = {}
+        j = 0
+        print(arr)
+        for i in range(0,len(arr)):
+            print('i:',i)
+            if arr[i] in m:
+                m[arr[i]]+=1
+            else:
+                m[arr[i]]=1
+            print(m)
+            while len(m)>2 and j<=i:
+                del m[arr[i]]
+                j+=1    
+                
+            
+            print(m)
+        print(m)
+        print('--end--')
+        return i-j+1 # len(m) is wrong, always be 2
 
-
-        return
-
-if __name_ _ == '__main__':
-    arr = []
+if __name__ == '__main__':
+    #arr = 'eceba'
+    arr = 'ccaabbb'
     sol = Solution()
     print(sol.findLength(arr))
 
