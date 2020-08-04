2af8cb7a7e5619557881a64978bae404be18ef6b
cr.ke <crke@digwise-tech.com>
2020-08-04 02:37:31 -0700

    update std

--- Sliding_Window/7_Longest_Substring_with_At_Most_Two_Distinct_Characters/m.py
+++ Sliding_Window/7_Longest_Substring_with_At_Most_Two_Distinct_Characters/m.py
@@ -5,29 +5,40 @@ class Solution(object):
         rtype: int
         """
         # Caterpillar
+        res = 0
         m = {}
         j = 0
         print(arr)
-        for i in range(0,len(arr)):
-            print('i:',i)
+        n = len(arr)
+        for i in range(0, n): # Make Big O more clear
+            print('---loop start i:',i)
+            
             if arr[i] in m:
                 m[arr[i]]+=1
             else:
                 m[arr[i]]=1
-            print(m)
-            while len(m)>2 and j<=i:
-                del m[arr[i]]
-                j+=1    
-                
             
+            print('before while')
+            print('m:', m)
+            while len(m) > 2 and j <= i:
+                print('inside while')
+                m[arr[j]]-=1
+                if m[arr[j]]==0:
+                    del m[arr[j]]
+                j+=1    
+            res = max(res, i-j+1)   
+            print('i,j:',i,j)
+            print('after while')
             print(m)
+            
+            print('---loop once---')
         print(m)
         print('--end--')
-        return i-j+1 # len(m) is wrong, always be 2
+        return res # len(m) is wrong, always be 2
 
 if __name__ == '__main__':
-    #arr = 'eceba'
-    arr = 'ccaabbb'
+    arr = 'eceba'
+    #arr = 'ccaabbb'
     sol = Solution()
     print(sol.findLength(arr))
 
