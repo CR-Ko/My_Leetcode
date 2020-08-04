5c21e5e47d5329c3ea30fbe7b7bcfe52febf4b10
cr.ke <crke@digwise-tech.com>
2020-08-04 03:14:20 -0700

    two points begin

---
+++ Two_points/1_Print_all_pairs_with_given_sum/std.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+using namespace std;
+
+class Solution {
+	public:
+		static int findXXX(int S, const vector<int>& arr){
+				
+		}
+};
+
+int main(int argc, char* argv[]){
+	int result = Solution::findXXX(7, vector<int>{2, 1, 5, 2, 3, 2});
+	cout << " : " << result << endl;
+}
+
+
+
---
+++ Two_points/1_Print_all_pairs_with_given_sum/std.py
@@ -0,0 +1,34 @@
+"""
+Given an array of integers, and a number ‘sum’, print all pairs in the array whose sum is equal to ‘sum’.
+
+Examples :
+Input  :  arr[] = {1, 5, 7, -1, 5},
+          sum = 6
+Output : (1, 5) (7, -1) (1, 5)
+
+Input  :  arr[] = {2, 5, 17, -1},
+          sum = 7
+Output :  (2, 5)
+"""
+
+
+class Solution(object):
+    def printPairs(self, arr, n, sum):
+        """
+        count = 0
+        Consider all possible pairs
+        and check their sums
+        """
+        for i in range(0, n):
+            for j in range(i+1, n):
+                if(arr[i]+arr[j]==sum):
+                    print('(', arr[i],', ', arr[j], '),', sep="")
+        #return
+
+if __name__ == '__main__':
+    arr = [1,5,7,-1,5]
+    n = len(arr)
+    sum = 6
+    sol = Solution()
+    sol.printPairs(arr, n, sum)
+
